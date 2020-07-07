3d4cdcdc063adc72dcdfc9adc4db3994bef2a735
Luis Eduardo Alcantara Olvera <geekshinobi@gmail.com>
2020-07-06 21:07:43 -0500

    Ceramo un controlador

---
+++ API/controllers/pacienteControllers.js
@@ -0,0 +1,7 @@
+
+
+// Cuando se crea un nuevo cliente
+exports.nuevoCliente = (res, req, next) => {
+    // TODO: insertar en la base de datos
+    resizeBy.json({mensaje: 'El cliente se agregó correctametne'});
+}
