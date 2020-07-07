18098ecb95c61626f241cf894100a337ee76e0b7
Luis Eduardo Alcantara Olvera <geekshinobi@gmail.com>
2020-07-06 21:33:43 -0500

    Solucionamos errores y realizamos insert a la base de datos

--- API/controllers/pacienteControllers.js
+++ API/controllers/pacienteControllers.js
@@ -1,8 +1,18 @@
-
+const Paciente = require('../models/Paciente');
 
 // Cuando se crea un nuevo cliente
-exports.nuevoCliente = (req, res, next) => {
+exports.nuevoCliente = async (req, res, next) => {
     console.log(req.body);
-    // TODO: insertar en la base de datos
-    res.json ({ mensaje: 'El cliente se agregó correctametne'});
+    // crear objeto de paciente con datos de req.body
+    const paciente = new Paciente(req.body);
+
+    try {
+        await paciente.save();
+        res.json ({ mensaje: 'El cliente se agregó correctametne'});
+    } catch (error) {
+        console.log(error);
+        next();
+    }
+    
+   
 }
